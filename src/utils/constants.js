9d00fdb13174678362078d3a9386532094b9da0d
tquandoo_24 <quantrungdo2401@gmail.com>
2024-04-27 15:47:10 +0700

    update api_root

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,7 +4,7 @@ if (process.env.BUILD_MODE === 'dev') {
     apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
-    apiRoot = 'https://trello-web-backend.onrender.com/'
+    apiRoot = 'https://trello-web-backend.onrender.com'
 }
 console.log('🚀 ~ apiRoot:', apiRoot)
 
