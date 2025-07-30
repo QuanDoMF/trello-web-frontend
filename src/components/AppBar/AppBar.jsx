8edf2c3636434302e9a37d0376ba0192ea342133
QuanDoMF <quantrungdo2401@gmail.com>
2025-07-30 21:11:49 +0700

    fix some bug

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -44,7 +44,11 @@ const AppBar = () => {
       }}
     >
       <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-        <AppsIcon sx={{ color: "white" }} />
+        <Link to="/boards">
+          <Tooltip title="Board List">
+            <AppsIcon sx={{ color: "white", verticalAlign: "middle" }} />
+          </Tooltip>
+        </Link>
         <Link to="/">
           <Box sx={{ display: "flex", alignItems: "center", gap: 0.5 }}>
             <SvgIcon
